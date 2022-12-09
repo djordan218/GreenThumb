34c11b0be5836a0fcc8f7257d7b74c1260c4a7a2
Daniel Jordan <danieljordan@Daniels-MacBook-Pro.local>
2022-12-09 16:39:40 -0600

    updated

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const HOST = process.env.RDS_HOST || 'localhost';
 const DATABASE = process.env.RDS_DATABASE || 'green_thumb';
 const PORT = process.env.RDS_PORT || 5432;
 
-// app.listen(LOCAL_PORT, function () {
-//   console.log(`Started on http://localhost:${LOCAL_PORT}`);
-//   console.log(
-//     `DATABASE: postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`
-//   );
-// });
+app.listen(LOCAL_PORT, function () {
+  console.log(`Started on http://localhost:${LOCAL_PORT}`);
+  console.log(
+    `DATABASE: postgres://${USER}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}`
+  );
+});
 
 module.exports.handler = serverless(app);
